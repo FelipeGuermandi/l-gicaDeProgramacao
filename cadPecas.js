1c395ccbad89499a9f17e8e6eb1a9b0b9d07d36c
FelipeGuermandi <107571781+FelipeGuermandi@users.noreply.github.com>
2022-07-06 06:22:20 -0300

    Add files via upload

---
+++ cadPecas.js
@@ -0,0 +1,15 @@
+let nomePeca = "carro";
+let numeroDePecas = 12;
+let pesoPeca = 150;
+
+if(pesoPeca >= 100){
+    console.log("Pode cadastrar.");
+}
+
+if(numeroDePecas >= 10){
+    console.log("Caixa cheia.");
+}
+
+if(nomePeca.length < 3){
+    console.log("Nome inválido!");
+}
