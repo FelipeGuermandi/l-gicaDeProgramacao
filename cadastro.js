264d87f98e9ee5a0f008072f3ac6850788c8b17c
FelipeGuermandi <107571781+FelipeGuermandi@users.noreply.github.com>
2022-07-20 05:21:05 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,16 @@
+let nome = "Felipe";
+let idade = 35;
+let email = "lipe.guermandi@gmail.com";
+let qtdLista = 90;
+
+if (idade >= 18){
+    if (qtdLista < 100){
+        console.log("Cadastro realizado.");   
+    }
+    else{
+        console.log("A lista já está cheia.");
+    }
+}
+else{
+    console.log("Idade mínima de 18 anos.");
+}
